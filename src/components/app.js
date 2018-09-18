f499795516091ffbc34b8822180ed509e4558d15
MjMajorMajorMj <techk8@gmail.com>
2018-09-18 11:15:05 -0700

    Created basic header and non-functioning form for posts

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import './app.css';
+import SubmitPost from './submitPost';
 
+export default props => {
+    return (
+        <div className="threadHeader">
+            <h1 className="container text-center">React Textboard</h1>
+            <h3 className="text-center">Thread Name!</h3>
+            <SubmitPost />
+        </div>
+    )
+}
 
---
+++ src/components/submitPost.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './submitPost.css';
+
+class SubmitPost extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <form className="submitPostForm text-center">
+                <div className="form-group">
+                    <label className="replyLabel">Reply</label>
+                    <input name="replyPost" type="text" className="form-control"/>
+                </div>
+                <button className="submitPostBtn btn btn-outline-light">Submit</button>
+            </form>
+        )
+    };
+};
+
+export default SubmitPost;

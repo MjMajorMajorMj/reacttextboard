3f9df1e8f257496ecf591b8aa79f6ece70b8a8e2
MjMajorMajorMj <techk8@gmail.com>
2018-09-18 15:58:42 -0700

    Created basic form adding

--- src/components/submitPost.js
+++ src/components/submitPost.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import './submitPost.css';
+import Field from './fieldReply';
 
 class SubmitPost extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            form: {
+                replyMsg: ''
+            }
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+    handleInputChange(event) {
+        const { value, name } = event.target;
+        const { form } = this.state;
+        form[name] = value;
+        this.setState({
+            form: {...form}
+        });
+    };
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.add(this.state.form);
     }
     render() {
+        const { replyMsg } = this.state.form;
         return (
-            <form className="submitPostForm text-center">
-                <div className="form-group">
-                    <label className="replyLabel">Reply</label>
-                    <input name="replyPost" type="text" className="form-control"/>
-                </div>
+            <form className="submitPostForm text-center" onSubmit={this.handleSubmit}>
+                <Field name="replyMsg" label="Reply" type="text" value={replyMsg} onChange={this.handleInputChange}/>
                 <button className="submitPostBtn btn">Submit</button>
             </form>
         )

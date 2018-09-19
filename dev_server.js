7a0963c00f2383318d383e113bc46306884ad74c
MjMajorMajorMj <techk8@gmail.com>
2018-09-19 12:07:24 -0700

    Changed main base to allow use of PHP

--- dev_server.js
+++ dev_server.js
@@ -1,22 +1,26 @@
 const { resolve } = require('path');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const config = require('./webpack.config');
+const config = require('./webpack.dev');
+const npm_config = require('./package.json');
 const PORT = process.env.PORT || 3000;
 
 new WebpackDevServer(webpack(config), {
-    contentBase: resolve(__dirname, 'public', 'dist'),
-    publicPath: '/',
+    contentBase: resolve(__dirname, 'public'),
+    publicPath: '/dist/',
     hot: false,
     historyApiFallback: true,
     quiet: false,
     noInfo: false,
+    proxy: npm_config.proxy,
     stats: {
         assets: false,
         colors: true,
         version: false,
         hash: false,
-        timings: false,
+        timings: true,
+        children: false,
+        modules: false,
         chunks: false,
         chunkModules: false
     }
@@ -26,5 +30,5 @@ new WebpackDevServer(webpack(config), {
     }
 
     console.log('\x1b[36m%s\x1b[33m%s\x1b[0m', 'Dev server running at ', 'localhost:' + PORT);
-    console.log('\x1b[32m%s\x1b[0m', '\nWebpack compiling...');
+    console.log('\x1b[32m%s\x1b[0m', '\nWebpack compiling...\n');
 });

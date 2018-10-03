e2a017a7781234cb342193d344dbddf8ff947a4a
MjMajorMajorMj <techk8@gmail.com>
2018-10-03 10:44:52 -0700

    Added basic functional dynamic routing for threads

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import Thread from './thread';
 import Board from './board';
-import { Route } from 'react-router-dom';
 
 const App = () => (
     <div>
         <h1 className="container text-center titleText">React Textboard</h1>
-        <Route exact path='/' component={Board}></Route>
-        <Route
-            path="/test"
-            render={(props) => <Thread {...props} threadID='testthread' />}
-        />
+        <hr></hr>
+        <Board />
     </div>
-    //<Thread threadID='testthread' />
 );
 
 export default App;
